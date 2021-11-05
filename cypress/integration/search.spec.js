478c8d4336722cc043a9685c704155adfc996d8c
Smilly Delmondes <smilly.3d@gmail.com>
2021-11-04 21:03:47 -0300

    implement third test

---
+++ cypress/integration/search.spec.js
@@ -0,0 +1,8 @@
+context("Search", () => {
+  it("Enters in landing page and search cep", () => {
+    cy.visit("http://localhost:3000");
+    cy.viewport(1440, 900);
+    cy.get("input[type=number]").type(65054310);
+    cy.get("button").click();
+  });
+});
